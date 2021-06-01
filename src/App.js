8d491c76e71b649051aa4163c84fb659ab0b24b5
nick plechas <nickplechas@gmail.com>
2021-06-01 11:44:00 -0700

    tempy

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Live Workouts. Extensive On-Demand Library. At Home Fitness Community.
+        </p>
+
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
